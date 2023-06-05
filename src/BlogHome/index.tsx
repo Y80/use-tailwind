587063150a474bd3de0692b026cef46c706b8761
oki <t.cpp@outlook.com>
2023-06-05 14:08:34 +0800

    chore: add prettier-plugin-tailwindcss

--- src/BlogHome/index.tsx
+++ src/BlogHome/index.tsx
@@ -4,21 +4,21 @@ import avatar from '../assets/avatar.png'
 
 export default function BlogHome() {
   return (
-    <div className="bg-zinc-800 h-screen pt-32 text-white">
-      <main className="mx-auto w-[800px] flex">
-        <aside className="w-[160px] flex flex-col gap-8 shrink-0">
+    <div className="h-screen bg-zinc-800 pt-32">
+      <main className="mx-auto flex w-[800px]">
+        <aside className="flex w-40 shrink-0 flex-col gap-8">
           <div className="flex items-center">
-            <img src={avatar} className="w-16 rounded-full border-gray-600 border-2" />
-            <h1 className="grow text-center text-4xl font-bold">令川</h1>
+            <img src={avatar} className="w-16 rounded-full border-2 border-gray-600" />
+            <h1 className="grow text-center text-4xl font-bold text-white">令川</h1>
           </div>
           <div className="flex justify-between text-slate-200">
             <span className="flex items-center gap-2">
               <Icon icon="octicon:location-16" />
-              杭州
+              <span>杭州</span>
             </span>
             <span className="flex items-center gap-2">
               <Icon icon="ps:work-case" />
-              前端开发
+              <span>前端开发</span>
             </span>
           </div>
           <div className="flex justify-between text-2xl text-slate-200">
@@ -27,12 +27,12 @@ export default function BlogHome() {
             <Icon icon="bxl:tiktok" />
             <Icon icon="codicon:twitter" />
           </div>
-          <div className="text-xs text-slate-300 leading-5">
+          <div className="text-xs leading-5 text-slate-300">
             React · Vue · Tailwind CSS · Tauri · Rust · Raycast · VS Code · Figma · Fig · JSBox · Proxyman · Cloudflare
           </div>
           <footer className="text-xs text-slate-400">
             <div>Copyright © 2023 lccl.cc</div>
-            <div className="mt-2">京ICP备123000111号-5</div>
+            <div className="mt-2">京ICP备000111号-5</div>
           </footer>
         </aside>
 
@@ -43,20 +43,20 @@ export default function BlogHome() {
             { year: 2021, postCount: 3 },
           ].map(({ year, postCount }) => {
             return (
-              <div key={year}>
+              <section key={year}>
                 <h3 className="text-xl font-bold text-slate-200">{year}</h3>
-                {Array.from({ length: postCount }).map(() => {
+                {Array.from({ length: postCount }).map((_, idx) => {
                   return (
-                    <div className="mt-4 flex items-center gap-4">
-                      <span className="text-slate-400 flex-shrink-0 text-sm" style={{ fontFamily: 'Fira Code' }}>
-                        {faker.date.month({ abbreviated: true })}{' '}
+                    <div key={idx} className="mt-4 flex items-center gap-4">
+                      <span className="flex-shrink-0 text-sm text-slate-400" style={{ fontFamily: 'Fira Code' }}>
+                        {faker.date.month({ abbreviated: true }) + ' '}
                         {('0' + faker.number.int({ min: 1, max: 31 })).slice(-2)}
                       </span>
-                      <span className="text-slate-300 font-bold text-lg line-clamp-1">{faker.lorem.sentence()}</span>
+                      <span className="line-clamp-1 text-lg font-bold text-slate-300">{faker.lorem.sentence()}</span>
                     </div>
                   )
                 })}
-              </div>
+              </section>
             )
           })}
         </div>
